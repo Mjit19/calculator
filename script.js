a6e6e412ded989004e7a62b1c168605467217f06
Mjit19 <mjitmukherjee19@gmail.com>
2023-10-31 13:34:01 +0530

    Add the main operate function

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+let num1, num2, operator;
+
+function operate(sign, a, b) {
+    if (sign == "+") {
+        return add(a, b);
+    } else if (sign == "-") {
+        return subtract(a, b);
+    } else if (sign == "*") {
+        return multiply(a, b);
+    } else if (sign == "/") {
+        return divide(a, b);
+    }
+    
+}
+
 function add(a, b) {
-    return a + b;
+    return (a + b).toFixed(2);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return (a - b).toFixed(2);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return (a * b).toFixed(2);
 }
 
 function divide(a, b) {
-    return a / b;
+    return (a / b).toFixed(2);
 }
