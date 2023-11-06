c3d974f57ee74c9a69dc6ad132443df9426020ae
Mjit19 <mjitmukherjee19@gmail.com>
2023-11-06 11:42:50 +0530

    Fix issues caused by null value
    Calculator would have glitches when given the null value
    Changed it to empty value to make it work almost the same
    as to when null was assigned

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let num1, num2, operator, result;
+let num1 = '', num2 = '', operator = '', result = '';
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 
@@ -11,14 +11,14 @@ const point = document.querySelector('#point');
 digits.forEach(digit => {
     digit.addEventListener('click', () => {
         if (lowerDisplay.textContent.length >= 16) {
-            num1 = null;
-            num2 = null;
+            num1 = '';
+            num2 = '';
             return lowerDisplay.textContent = "Error";
         }
-        if(lowerDisplay.textContent === "Error" || result != null) {
+        if(lowerDisplay.textContent === "Error" || result != '') {
             lowerDisplay.textContent = '';
             upperDisplay.textContent = '';
-            result = null;
+            result = '';
         }
         if (lowerDisplay.textContent != '0' || digit.textContent != '0'){
             lowerDisplay.textContent += digit.textContent;
@@ -33,8 +33,8 @@ operators.forEach(sign => {
             return lowerDisplay.textContent = "Error";
         }
         if (lowerDisplay.textContent != '') {
-            num1 = Number(lowerDisplay.textContent);
-            result = null;
+            num1 = lowerDisplay.textContent;
+            result = '';
         }
         operator = sign.textContent;
         upperDisplay.textContent = num1 + " " + operator + " ";
@@ -43,37 +43,37 @@ operators.forEach(sign => {
 })
 
 equals.addEventListener('click', () => {
-    if (num1 != null && lowerDisplay.textContent != '') {
-        num2 = Number(lowerDisplay.textContent);
+    if (num1 != '' && lowerDisplay.textContent != '') {
+        num2 = lowerDisplay.textContent;
     }
-    if(num2 == null) {
+    if(num2 == '') {
         upperDisplay.textContent = '';
-        num1 = null;    // Sets num1 to null if num1 has value
+        num1 = '';    // Sets num1 to null if num1 has value
         return lowerDisplay.textContent = "Error";
     }
 
     if (operator == "/" && num2 == 0) {
         upperDisplay.textContent = '';
-        num1 = null;
-        num2 = null;
+        num1 = '';
+        num2 = '';
         return lowerDisplay.textContent = "Error"
     }
     upperDisplay.textContent += " " + num2 + " = ";
-    result = (Math.round(operate(operator, num1, num2) * 100) / 100).toString();
+    result = (Math.round(operate(operator, Number(num1), Number(num2)) * 100) / 100).toString();
     
     if (result.length < 16) {
         lowerDisplay.textContent = result;
     } else {
         lowerDisplay.textContent = "Error";
     }
-    num1 = null, num2 = null, operator = null;
+    num1 = '', num2 = '', operator = '';
     
 })
 
 clear.addEventListener('click', () => {
     upperDisplay.textContent = "";
     lowerDisplay.textContent = "";
-    num1 = null, num2 = null, operator = null;
+    num1 = '', num2 = '', operator = '';
 })
 
 point.addEventListener('click', () => {
